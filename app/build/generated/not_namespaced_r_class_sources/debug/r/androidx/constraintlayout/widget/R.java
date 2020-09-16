2612e97ba51bc369fbfd5ee853e864e6b8b5126b
desmondykl <42940055+desmondykl@users.noreply.github.com>
2020-09-16 18:33:55 +0800

    UI_Update

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f09009a;
         public static final int invisible = 0x7f0900a6;
         public static final int left = 0x7f0900ab;
-        public static final int packed = 0x7f0900cf;
-        public static final int parent = 0x7f0900d3;
-        public static final int percent = 0x7f0900d6;
-        public static final int right = 0x7f0900e0;
-        public static final int spread = 0x7f09010c;
-        public static final int spread_inside = 0x7f09010d;
-        public static final int start = 0x7f090112;
+        public static final int packed = 0x7f0900d0;
+        public static final int parent = 0x7f0900d4;
+        public static final int percent = 0x7f0900d7;
+        public static final int right = 0x7f0900e1;
+        public static final int spread = 0x7f09010d;
+        public static final int spread_inside = 0x7f09010e;
+        public static final int start = 0x7f090113;
         public static final int top = 0x7f090131;
-        public static final int wrap = 0x7f090147;
+        public static final int wrap = 0x7f090146;
     }
     public static final class styleable {
         private styleable() {}
